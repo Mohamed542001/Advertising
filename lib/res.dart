729029d35f8a2dd15baf2a91f2398a6df60ddf22
Mohamed Salama <01550526487m@gmail.com>
2021-11-05 15:17:32 +0200

    first1

--- lib/res.dart
+++ lib/res.dart
@@ -1,6 +1,6 @@
 class Res{
 static const String splash = "assets/images/splash.jpg";
-static const String logo = "assets/images/logo.jpg";
+static const String logo = "assets/images/logo.png";
 static const String stad1 = "assets/images/stad1.JPG";
 static const String stad2 = "assets/images/stad2.jpeg";
 static const String stad3 = "assets/images/stad3.jpg";
